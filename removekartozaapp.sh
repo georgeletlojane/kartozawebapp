2b49378379561d1a217cc8b1e35a89bb24bb9060
George Letlojane <69802565+georgeletlojane@users.noreply.github.com>
2020-11-25 12:53:49 +0200

    Update removekartozaapp.sh
    Script to remove the Kartoza Web App

--- removekartozaapp.sh
+++ removekartozaapp.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
+# Author: George Letlojane (SG)
+# Revised: 25/11/2020
 
+## Usage: ./deploykartoza.sh YOURSTACKNAME
+
+if [ $# -eq 0 ]
+  then
+        echo "No stack name specified"
+        echo "USAGE: ./deploykartoza.sh YOURSTACKNAME"
+        exit 1
+fi
+
+echo "Removing the stack..."
+docker stack rm $1
+
+echo "Removing node from swarm..."
+docker swarm leave --force
+
+echo "Done."
+
+# That's all
